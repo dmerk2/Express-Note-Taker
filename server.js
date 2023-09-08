2477540ba9328b177d3c3979ed28f7fe2d0ab2be
Dan Merkin <dan.merkin@gmail.com>
2023-09-08 13:52:19 -0400

    Adding wildcard route for mistyped URL

---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const path = require("path");
+const { clog } = require('./middleware/clog');
+const api = require("./routes/index")
+
+// Assign PORT variable for heroku port otherwise run on 3001
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// Import custom middleware, "cLog"
+app.use(clog);
+
+// Middleware for parsing JSON and urlencoded form data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use('/api', api);
+
+app.use(express.static("public"));
+
+// GET route for homepage
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "/public/index.html"));
+});
+
+// Get route for notes page
+app.get("/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "/public/notes.html"));
+});
+
+// Adding wildcard route
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "/public/404.html"))
+})
+
+// Listen for the port
+app.listen(PORT, () => {
+  console.log(`App listening at http://localhost:${PORT}`);
+});
